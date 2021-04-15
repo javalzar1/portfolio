cff323495d3ef75520170a7be0cbb1b84c25e40a
Javier Zarate <javalzar17@gmail.com>
2021-04-15 16:33:52 -0700

    added custom nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<section className='s1'>
+				<div className='main-container'>
+					<div className='greeting-wrapper'>
+						<h1>Hi, I'm Javier Zarate</h1>
+					</div>
+
+					<div className='intro-wrapper'>
+						<div className='nav-wrapper'>
+							<div className='dots-wrapper'>
+								<div id='dot-1' className='browser-dot'></div>
+								<div id='dot-2' className='browser-dot'></div>
+								<div id='dot-3' className='browser-dot'></div>
+							</div>
+
+							<ul id='navigation'>
+								<li>
+									<a href='#'>About</a>
+								</li>
+								<li>
+									<a href='#'>Porfolio</a>
+								</li>
+							</ul>
+						</div>
+						<div className='left-column'>left-column</div>
+						<div className='right-column'>right-column</div>
+					</div>
+				</div>
+			</section>
+
+			<section className='s2'>
+				<div className='main-container'>About</div>
+			</section>
+
+			<section className='s1'>
+				<div className='main-container'>Portfolio</div>
+			</section>
+
+			<section className='s2'>
+				<div className='main-container'>Contact</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
