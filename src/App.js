581b587103c7dc2c28d023c9663815d0f8feb181
Javier Zarate <javalzar17@gmail.com>
2021-04-15 18:40:06 -0700

    added small about sectiona and completed mac window look

--- src/App.js
+++ src/App.js
@@ -26,8 +26,26 @@ function App() {
 								</li>
 							</ul>
 						</div>
-						<div className='left-column'>left-column</div>
-						<div className='right-column'>right-column</div>
+
+						<div className='left-column'>
+							<img id='profile-pic' src='./assets/profile.jpg' alt='profile' />
+						</div>
+
+						<div className='right-column'>
+							<div id='preview-shadow'>
+								<div className='preview'>
+									<div id='corner-tl' className='corner'></div>
+									<div id='corner-tr' className='corner'></div>
+									<h3>Software Engineer</h3>
+									<p>
+										A master is always a student. Software Engineer learning something new every
+										day!
+									</p>
+									<div id='corner-bl' className='corner'></div>
+									<div id='corner-br' className='corner'></div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</section>
