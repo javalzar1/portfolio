e0c7be5cdd53df99ae8b9e1b9e528f2307470145
Javier Zarate <javalzar17@gmail.com>
2021-04-29 15:50:28 -0700

    intro section fully converted

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,11 +19,11 @@ const IntroWrapper = styled.div`
 	margin-top: 25px;
 
 	${mediaQueries('desktop1')`
-	grid-template-columns: 1fr;
-	grid-template-areas:
-		'nav-wrapper'
-		'left-column'
-		'right-column';
+		grid-template-columns: 1fr;
+		grid-template-areas:
+			'nav-wrapper'
+			'left-column'
+			'right-column';
 	`};
 `;
 
@@ -54,6 +54,15 @@ const Navigation = styled.ul`
 	a {
 		color: black;
 	}
+
+	${mediaQueries('mobile1')`
+		font-size: 15px;
+	`}
+
+	${mediaQueries('mobile2')`
+		font-size: 10px;
+		font-weight: 600;
+	`}
 `;
 
 const LeftColumn = styled.div`
@@ -61,6 +70,10 @@ const LeftColumn = styled.div`
 	padding-top: 100px;
 	padding-bottom: 50px;
 	background-color: #fff;
+
+	${mediaQueries('mobile1')`
+		padding-top: 50px;
+	`};
 `;
 
 const RightColumn = styled.div`
@@ -75,6 +88,114 @@ const RightColumn = styled.div`
 		justify-content: center;
 		padding-bottom: 100px;
 	`};
+
+	${mediaQueries('mobile1')`
+		padding-top: 0;
+	`};
+`;
+
+const DotsWrapper = styled.div`
+	display: flex;
+	padding: 10px;
+`;
+
+const BrowserDot = styled.div`
+	height: 15px;
+	width: 15px;
+	border-radius: 50%;
+	margin: 5px;
+
+	box-shadow: -1px 1px 3px -1px rgba(0, 0, 0, 0.75);
+	-webkit-box-shadow: -1px 1px 3px -1px rgba(0, 0, 0, 0.75);
+	-moz-box-shadow: -1px 1px 3px -1px rgba(0, 0, 0, 0.75);
+
+	&#dot-1 {
+		background-color: #fc6058;
+	}
+	&#dot-2 {
+		background-color: #fec02f;
+	}
+	&#dot-3 {
+		background-color: #2aca3e;
+	}
+`;
+
+const ProfilePic = styled.img`
+	display: block;
+	margin: 0 auto;
+	height: 300px;
+	width: 230px;
+	object-fit: fill;
+	border: 2px solid var(--borderColor);
+`;
+
+const PreviewShadow = styled.div`
+	background-color: var(--previewShadow);
+	width: 280px;
+	height: 180px;
+	padding-left: 30px;
+	padding-top: 30px;
+
+	${mediaQueries('mobile1')`
+		padding-top: 15px;
+		padding-left: 15px;
+	`};
+
+	${mediaQueries('mobile2')`
+		width: 180px;
+		height: 180px;
+		padding-left: 30px;
+		padding-top: 30px;
+	`};
+`;
+
+const Preview = styled.div`
+	width: 280px;
+	border: 1.5px solid var(--borderColor);
+	background-color: var(--previewBg);
+	padding: 15px;
+	position: relative;
+
+	${mediaQueries('mobile1')`
+		width: 260px;
+	`};
+
+	${mediaQueries('mobile2')`
+		width: 180px;
+
+		h3 {
+			font-size: 21px;
+		}
+	`};
+`;
+
+const Corner = styled.div`
+	width: 7px;
+	height: 7px;
+	border: 1.5px solid var(--borderColor);
+	border-radius: 50%;
+	background-color: #fff;
+	position: absolute;
+
+	&#corner-tl {
+		top: -5px;
+		left: -5px;
+	}
+
+	&#corner-tr {
+		top: -5px;
+		right: -5px;
+	}
+
+	&#corner-bl {
+		bottom: -5px;
+		left: -5px;
+	}
+
+	&#corner-br {
+		bottom: -5px;
+		right: -5px;
+	}
 `;
 
 const Intro = () => {
@@ -83,11 +204,11 @@ const Intro = () => {
 			<div className='main-container'>
 				<IntroWrapper>
 					<NavWrapper>
-						<div className='dots-wrapper'>
-							<div id='dot-1' className='browser-dot'></div>
-							<div id='dot-2' className='browser-dot'></div>
-							<div id='dot-3' className='browser-dot'></div>
-						</div>
+						<DotsWrapper>
+							<BrowserDot id='dot-1'></BrowserDot>
+							<BrowserDot id='dot-2'></BrowserDot>
+							<BrowserDot id='dot-3'></BrowserDot>
+						</DotsWrapper>
 
 						<Navigation>
 							<li>
@@ -111,24 +232,24 @@ const Intro = () => {
 					</NavWrapper>
 
 					<LeftColumn>
-						<img id='profile-pic' src='./assets/profile.jpg' alt='profile' />
+						<ProfilePic src='./assets/profile.jpg' alt='profile' />
 					</LeftColumn>
 
 					<RightColumn>
-						<div id='preview-shadow'>
-							<div className='preview'>
-								<div id='corner-tl' className='corner'></div>
-								<div id='corner-tr' className='corner'></div>
+						<PreviewShadow>
+							<Preview>
+								<Corner id='corner-tl'></Corner>
+								<Corner id='corner-tr'></Corner>
 								<h3>My name is Javier</h3>
 								<p>
 									I am a full-stack software engineer. I am eager to be challenged and ready to work.
 									Let's build something together! Feel free to <a href='#contact'>reach out</a>.
 								</p>
 								<p></p>
-								<div id='corner-bl' className='corner'></div>
-								<div id='corner-br' className='corner'></div>
-							</div>
-						</div>
+								<Corner id='corner-bl'></Corner>
+								<Corner id='corner-br'></Corner>
+							</Preview>
+						</PreviewShadow>
 					</RightColumn>
 				</IntroWrapper>
 			</div>
--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -2,7 +2,6 @@ export const breakpoints = {
 	desktop1: 800,
 	desktop2: 900,
 	desktop3: 1000,
-	desktop4: 1200,
 	mobile1: 475,
 	mobile2: 360,
 };
